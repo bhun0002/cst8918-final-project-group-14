3f5fc563ccf88bb1223051b232692470441b4518
bhun0002 <bhungaliyadeval@gmail.com>
2025-04-14 08:12:46 -0400

    Add GitHub Actions CI/CD workflows and OIDC-based Terraform provider setup

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -1,9 +1,3 @@
-module "backend" {
-  source          = "../../backend"
-  resource_prefix = var.resource_prefix
-  location        = var.location
-}
-
 module "network" {
   source          = "../../network"
   resource_prefix = var.resource_prefix
